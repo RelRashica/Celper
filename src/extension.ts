8b04439df0cd8e5c4765c00a4a1c1299823d3816
RelRashica <rashicanjemezero@gmail.com>
2026-03-24 17:48:21 +0100

    Added C++ support

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,11 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage('Celper is awake, stop worrying!');
 	console.log('DEBUG: Activate function is running!');
 
-    // 1. Create a "Diagnostic Collection" (The container for our red lines)
     const diagnosticCollection = vscode.languages.createDiagnosticCollection('celper');
     context.subscriptions.push(diagnosticCollection);
 
-    // 2. The Listener: This runs every time you save a file
     let onSave = vscode.workspace.onDidSaveTextDocument((document) => {
-        // Only run if it's a C file
-        if (document.languageId === 'c') {
+        if (document.languageId === 'c' || document.languageId === 'cpp') {
 			console.log("FILE SAVED: " + document.fileName);
             runCelperLogic(document, diagnosticCollection);
         }
@@ -21,7 +18,6 @@ export function activate(context: vscode.ExtensionContext) {
 function runCelperLogic(doc: vscode.TextDocument, collection: vscode.DiagnosticCollection) {
     const diagnostics: vscode.Diagnostic[] = [];
     
-    // Key = variable name (ptr), Value = line number (4)
     let activeAllocations = new Map<string, number[]>();
     let isInComment = false;
 
@@ -34,33 +30,29 @@ function runCelperLogic(doc: vscode.TextDocument, collection: vscode.DiagnosticC
         if (text.includes("*/")) {isInComment = false; continue;}
 
         if (!isInComment) {
-            // TRACK MALLOC: Save the name AND the line index
             let mallocMatch = text.match(/(\w+)\s*=\s*malloc/);
             if (mallocMatch) {
     			let varName = mallocMatch[1];
     			let lines = activeAllocations.get(varName) || [];
-    			lines.push(i); // Add this new line to the list
+    			lines.push(i); 
     			activeAllocations.set(varName, lines);
 			}
 
-            // CLEAR FREE: Remove it if it gets freed
             let freeMatch = text.match(/free\s*\(\s*(\w+)\s*\)/);
             if (freeMatch) {
     			let varName = freeMatch[1];
     			let lines = activeAllocations.get(varName);
     			if (lines && lines.length > 0) {
-        			lines.pop(); // Remove the most recent malloc
+        			lines.pop(); 
         			if (lines.length === 0) {activeAllocations.delete(varName);}
     			}
 			}
 
-			// 4. RETURN CHECKER: If we return now, did we leave anything behind?
 			if (text.startsWith("return")) {
     			activeAllocations.forEach((linesArray: number[], varName: string) => {
-        			// If the variable in the map hasn't been freed yet...
         			if (linesArray.length > 0) {
             			const diag = new vscode.Diagnostic(
-                			line.range, // The line with the 'return'
+                			line.range,
                 			`💀 CELPER FATAL: You're trying to return, but '${varName}' is still leaked!`,
                 			vscode.DiagnosticSeverity.Error
             			); diagnostics.push(diag);
@@ -70,15 +62,12 @@ function runCelperLogic(doc: vscode.TextDocument, collection: vscode.DiagnosticC
         }
     }
 
-    // We loop through each line number in that array
     activeAllocations.forEach((linesArray: number[], varName: string) => {
     
-    // We loop through each line number in that array
     	linesArray.forEach((lineNum: number) => {
         	const line = doc.lineAt(lineNum);
         	let startChar = line.text.indexOf("malloc");
         
-        	// Fallback if malloc isn't found on that line
         	if (startChar === -1) {startChar = 0;} 
         	const endChar = startChar + 6;
 
